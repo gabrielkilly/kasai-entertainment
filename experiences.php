d4a25a769abab1ad3aff2791656c68a361a9e024
gabykilly@gmail.com <gabykilly@gmail.com>
2019-01-10 13:58:43 +0100

    Experiences page update

--- experiences.php
+++ experiences.php
@@ -5,31 +5,57 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="/assets/css/styles.min.css">
+    <link rel="stylesheet" href="/third-party/magnific-popup/magnific-popup.css">
     <link rel="icon" href="/assets/icons/kasai_flame.png">
 </head>
 <body id="experiences">
   <?php include __DIR__."/partials/navbar.php"?>
   <div class="grid">
-    <div class="grid-item"><img src="/assets/images/fire-dancer.jpg" /></div>
-    <div class="grid-item"><iframe id="yt_0" src="http://www.youtube.com/embed/Ce8142lbWv4?enablejsapi=1" frameborder="0" allowfullscreen></iframe></div>
-    <div class="grid-item"><img src="/assets/images/holiday-party-2.JPG" /></div>
-    <div class="grid-item"><img src="/assets/images/wedding-experience.jpg" /></div>
-    <div class="grid-item"><iframe id="yt_1" src="http://www.youtube.com/embed/bSGEExssB8M?enablejsapi=1" frameborder="0" allowfullscreen></iframe></div>
-    <div class="grid-item"><img src="/assets/images/dance-experience.jpg" /></div>
-    <div class="grid-item"><iframe id="yt_2" src="http://www.youtube.com/embed/Fu3KUBjAZiw?enablejsapi=1" frameborder="0" allowfullscreen></iframe></div>
-    <div class="grid-item"><img src="/assets/images/tavern-event.JPG" /></div>
-    <div class="grid-item"><img src="/assets/images/lady-dancer2.JPG" /></div>
-    <div class="grid-item"><iframe id="yt_3" src="http://www.youtube.com/embed/IO98fuN__YY?enablejsapi=1" frameborder="0" allowfullscreen></iframe></div>
-    <div class="grid-item"><img src="/assets/images/holiday-party-crew.jpg" /></div>
-    <div class="grid-item"><iframe id="yt_5" src="http://www.youtube.com/embed/Fu3KUBjAZiw?enablejsapi=1" frameborder="0" allowfullscreen></iframe></div>
-    <div class="grid-item"><iframe id="yt_4" src="http://www.youtube.com/embed/TccjoSYaOqM?enablejsapi=1" frameborder="0" allowfullscreen></iframe></div>
-    <div class="grid-item"><img src="/assets/images/lady-dancer.jpg" /></div>
-    <div class="grid-item"><img src="/assets/images/christmas-trees.JPG" /></div>
+    <div class="grid-item" data-mfp-src="/assets/images/fire-dancer.jpg">
+      <img src="/assets/images/fire-dancer.jpg" />
+    </div>
+    <div class="grid-item mfp-iframe" data-mfp-src="http://www.youtube.com/watch?v=Ce8142lbWv4">
+      <img src="https://img.youtube.com/vi/Ce8142lbWv4/maxresdefault.jpg" />
+    </div>
+    <div class="grid-item" data-mfp-src="/assets/images/holiday-party-2.JPG">
+      <img src="/assets/images/holiday-party-2.JPG" />
+    </div>
+    <div class="grid-item" data-mfp-src="/assets/images/wedding-experience.jpg">
+      <img src="/assets/images/wedding-experience.jpg" />
+    </div>
+    <div class="grid-item" data-mfp-src="/assets/images/dance-experience.jpg" >
+      <img src="/assets/images/dance-experience.jpg" />
+    </div>
+    <div class="grid-item mfp-iframe" data-mfp-src="http://www.youtube.com/watch?v=Fu3KUBjAZiw">
+      <img src="https://img.youtube.com/vi/Fu3KUBjAZiw/maxresdefault.jpg" />
+    </div>
+    <div class="grid-item" data-mfp-src="/assets/images/tavern-event.JPG" >
+      <img src="/assets/images/tavern-event.JPG" />
+    </div>
+    <div class="grid-item" data-mfp-src="/assets/images/lady-dancer2.JPG" >
+      <img src="/assets/images/lady-dancer2.JPG" />
+    </div>
+    <div class="grid-item mfp-iframe" data-mfp-src="http://www.youtube.com/watch?v=IO98fuN__YY">
+      <img src="https://img.youtube.com/vi/IO98fuN__YY/maxresdefault.jpg" />
+    </div>
+    <div class="grid-item" data-mfp-src="/assets/images/holiday-party-crew.jpg">
+      <img src="/assets/images/holiday-party-crew.jpg" />
+    </div>
+    <div class="grid-item mfp-iframe" data-mfp-src="http://www.youtube.com/watch?v=TccjoSYaOqM">
+      <img src="https://img.youtube.com/vi/TccjoSYaOqM/maxresdefault.jpg" />
+    </div>
+    <div class="grid-item" data-mfp-src="/assets/images/lady-dancer.jpg">
+      <img src="/assets/images/lady-dancer.jpg" />
+    </div>
+    <div class="grid-item" data-mfp-src="/assets/images/christmas-trees.JPG">
+      <img src="/assets/images/christmas-trees.JPG" />
+    </div>
   </div>
   <?php include __DIR__."/partials/footer.php"?>
   <script src="/third-party/jquery/jquery-3.3.1.min.js"></script>
   <script src="/third-party/jquery/jqeury-migrate-3.0.1.min.js"></script>
-  <script src="/third-party/masonry-docs/masonry.pkgd.min.js"></script>
+  <!-- <script src="/third-party/masonry-docs/masonry.pkgd.min.js"></script> -->
+  <script src="/third-party/magnific-popup/jquery.magnific-popup.min.js"></script>
   <script src="/assets/js/experiences.js"></script>
   <script src="/assets/js/main.js"></script>
 </body>
